ba0c08c90afb86c3162b97ff0f1311b2a6951647
Lucas Vinicius da Costa Santana <lvcs@WINDOWS.CIN.UFPE.BR>
2017-05-15 17:45:25 -0300

    lista 2

--- lista2/Q3/q3.m
+++ lista2/Q3/q3.m
@@ -1,12 +1,7 @@
-ker = [0, 0, 0; 0, 1, 0; 0, 0, 0];
+ker = 1/9 * ones(3);
 
 img = imread('imagem.jpg');
 before = uint8(rgb2gray(img));
 after = uint8(my_conv2d(before, ker));
 subplot(1, 2, 1), imshow(before);
 subplot(1, 2, 2), imshow(after);
-
-%{
-    As imagens obtidas são iguais, pois basicamantes estamos aplicando um
-    filtro que replica o pixel na sua posição.
-%}
--- lista2/Q4/q4.m
+++ lista2/Q4/q4.m
@@ -1,4 +1,4 @@
-ker = [0, 0, 0; 0, 1, 0; 0, 0, 0];
+ker = [-1, 0, 1; -2, 0, 2; -1, 0, 1];
 
 img = imread('imagem.jpg');
 before = uint8(rgb2gray(img));
@@ -6,7 +6,3 @@ after = uint8(my_conv2d(before, ker));
 subplot(1, 2, 1), imshow(before);
 subplot(1, 2, 2), imshow(after);
 
-%{
-    As imagens obtidas são iguais, pois basicamantes estamos aplicando um
-    filtro que replica o pixel na sua posição.
-%}
